942957d73fb582accda98636553ef9d4767d55d4
Costa Gonzalo <39466145@comunidadort.edu.ar>
2021-04-05 23:00:10 -0300

    Agrego funciones

--- script.js
+++ script.js
@@ -5,11 +5,57 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-
-const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+const list = document.getElementById('todo-list')
+
+let tareasTotales = 0
+let tareasNoRealizadas = 0
 
 function addTodo() {
-  alert('Boton Add TODO clickeado!')
+
+  const tarea = prompt("Ingrese una tarea");
+  if (tarea === null) {
+    return;
+  }
+
+  const division = document.createElement('div');
+
+  const li = document.createElement('li')
+  li.className = classNames.TODO_ITEM
+  
+
+  const check = document.createElement('input')
+  check.type = "checkbox"
+  check.className = classNames.TODO_CHECKBOX
+  check.onchange = listenerTareas
+
+  li.innerHTML = `${tarea}`
+  
+  li.appendChild(check)
+  division.appendChild(li)
+  list.appendChild(division)
+
+  contadorItems()
 }
+
+function contadorItems(){
+  tareasTotales++
+  tareasNoRealizadas++
+  itemCountSpan.innerHTML = tareasTotales
+  uncheckedCountSpan.innerHTML = tareasNoRealizadas
+}
+
+function listenerTareas(evento){
+
+  if(evento.target.checked){
+    tareasNoRealizadas--
+  }else{
+    tareasNoRealizadas++
+  }
+  uncheckedCountSpan.innerHTML = tareasNoRealizadas;
+}
+
+
+
+
